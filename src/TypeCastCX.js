b13c475853be47dbf2004448fa2947c5012cec56
darkknight13 <19567498+darkknight13@users.noreply.github.com>
2024-07-01 12:38:30 +0530

    core and dependency update

--- src/TypeCastCX.js
+++ src/TypeCastCX.js
@@ -1,5 +1,6 @@
 import { CX } from "@healthcare-interoperability/hl7v2-datatypes";
 import { HL7DataTypeCast } from "./HL7DataTypeCast";
+import { PrimitiveDataType } from "@healthcare-interoperability/hl7v2-core";
 
 
 export class TypeCastCX extends HL7DataTypeCast {
@@ -7,9 +8,11 @@ export class TypeCastCX extends HL7DataTypeCast {
         super(dataInstance);
     }
 
-    typecast() {
+    typecast(version) {
         if (this.dataInstance instanceof CX) {
             return this.dataInstance;
+        } else if (this.dataInstance instanceof PrimitiveDataType) {
+            return new CX(this._handlePrimitiveData(), this._prepareConfigs(version));
         } else {
             throw new Error(`Unsupported type for typecasting. Only CX is supported.`);
         }
